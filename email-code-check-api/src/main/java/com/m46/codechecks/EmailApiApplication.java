662532f4174636b68f44f65182806a771d037aa9
Sulumbek <9379992s>
2020-08-23 21:47:22 +0600

    Fixed pipeline

--- email-code-check-api/src/main/java/com/m46/codechecks/EmailApiApplication.java
+++ email-code-check-api/src/main/java/com/m46/codechecks/EmailApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.pivitengineering.store.model.entity"})
+@EntityScan(basePackages = {"com.m46.codechecks.model.entity"})
 public class EmailApiApplication {
 
 	public static void main(String[] args) {
--- question-check-api/src/main/java/com/m46/codechecks/CoreApiApplication.java
+++ question-check-api/src/main/java/com/m46/codechecks/CoreApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.pivitengineering.store.model.entity"})
 public class CoreApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoreApiApplication.class, args);
